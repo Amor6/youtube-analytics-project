f04ee3f04e7b74709be06250b62d5d8d0a690fdf
Amor6 <135590534+Amor6@users.noreply.github.com>
2023-07-02 15:50:16 +0400

    Nc2

--- src/channel.py
+++ src/channel.py
@@ -9,12 +9,12 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         # self.service = build('youtube', 'v3', developerKey='API_KEY')
-        # self.channel = self.service().channels().list(id=channel_id, part='snippet, statistics').execute()
+        self.channel = self.service().channels().list(id=channel_id, part='snippet, statistics').execute()
         self.channel_id = channel_id
         # self.api_key = os.getenv('API_KEY')
-        # self.title = self.channel["items"][0]['snippet']['title']
+        self.title = self.channel["items"][0]['snippet']['title']
         # self.description = self.channel['items'][0]['snippet']['description']
-        # self.url = 'http://www.youtube.com/channel/' + self.channel['items'][0]['id']
+        self.url = 'http://www.youtube.com/channel/' + self.channel['items'][0]['id']
         # self.subscribers_count = self.channel['items'][0]['statistics']['subscribersCount']
         # self.video_count = self.channel['items'][0]['statistics']['videoCount']
         # self.view_count = self.channel['items'][0]['statistics']['viewCount']
@@ -31,16 +31,57 @@ class Channel:
         return service
 
     def print_info(self, channel_id):
-
         r = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
 
-        print(r['items'][0]['snippet']['title'])
-        print(r['items'][0]['snippet']['publishedAt'])
-        print(r['items'][0]['statistics']['viewCount'])
+        #print(r['items'][0]['snippet']['title'])
+        #print(r['items'][0]['snippet']['publishedAt'])
+        #print(r['items'][0]['statistics']['viewCount'])
 
 
-    # def to_json(value: str):
-    # return getattr(value, Channel)
+     '''  Метод возвращающий название и ссылку на канал по шаблону <название_канала> (<ссылка_на_канал>) '''
+    def __str__(self):
+        return f'{self.title} ({self.url})'
 
-    # def channel(self) -> None:
-    # pass
+    ''' Метод сложения'''
+    def __add__(self, other):
+        return self.channel + other.channel
+
+    ''' Метод вычитания'''
+
+    def __sub__(self, other):
+         return self.channel - other.channel
+         return other.channel - self.channel
+
+    ''' Метод сравнивания в > сторону'''
+    def __gt__(self, other):
+        if self.channel > other.channel:
+            self.channel = False
+
+    ''' Метод сравнивания  где значения уточняется = или > значение у каналов '''
+    def __ge__(self, other):
+        if self.channel >= other.channel:
+            self.channel = False
+
+    ''' Метод сравнивания в < сторону'''
+    def __lt__(self, other):
+        if self.channel < other.channel:
+            self.channel = False
+
+    ''' Метод сравнивания  где значения уточняется = или < значение у каналов '''
+    def __le__(self, other):
+        if self.channel <= other.channel:
+            self.channel = False
+
+    def __eq__(self, other):
+        if self.channel == other.channel:
+            self.channel = False
+
+
+
+
+
+
+
+
+
+
