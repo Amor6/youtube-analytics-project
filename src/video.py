ab65b8c56cb06c74365a781db8d5612e93b42dd4
Amor6 <135590534+Amor6@users.noreply.github.com>
2023-07-16 12:08:52 +0400

    Nw 6

--- src/video.py
+++ src/video.py
@@ -3,21 +3,34 @@ from googleapiclient.discovery import build
 
 
 class Video:
-    api_key: str = os.getenv("YT_API_KEY")
+    api_key: str = "AIzaSyBIE1Zoz-q-0QXM1H8hp3e_N9xnuT_9xfI"
     youtube = build('youtube', 'v3', developerKey=api_key)
 
-    def __init__(self, video_id: str):
+    def __init__(self, video_id):
         self.video_id = video_id
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                         id=video_id).execute()
-        self.title = self.video_response['items'][0]['snippet']['title']
-        self.url_video = f"https://youtu.be/{self.video_id}"
-        self.viewCount = self.video_response['items'][0]['statistics']['viewCount']
-        self.likeCount = self.video_response['items'][0]['statistics']['likeCount']
+        self.video_id = video_id
+        self.title = None
+        self.url = None
+        self.view_count = None
+        self.like_count = None
+
+        try:
+            response = self.youtube.videos().list(
+                part='snippet, statistics',
+                id=self.video_id
+            ).execute()
+            video = response['items'][0]
+
+            self.title = video['snippet']['title']
+            self.url = "https://www.youtube.com/watch?v=" + self.video_id
+            self.view_count = video['statistics']['viewCount']
+            self.like_count = video['statistics']['likeCount']
+
+        except Exception as e:
+            print("Error:", str(e))
 
     def __str__(self):
-        return f'{self.title}'
-
+        return self.title
 
 class PLVideo(Video):
     def __init__(self, video_id: str, playlist_id: str):
